925e2504a76baa9b5f50caa402d464c052250c95
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-11-06 19:09:38 +0600

    ready to submit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className='bg-[#9538E2] text-center mb-48 pb-56 rounded-xl relative border   flex flex-col justify-center items-center'>
          <div className='md:w-3/4  p-10  mx-auto text-white space-y-4 '>
          <h3 className='text-3xl md:text-4xl'>Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h3>
-         <p>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+         <p>Explore the latest gadgets that will take your experience to , the next level. From smart devices to the coolest accessories, we have it all!</p>
          <Link to='/deshboard' className='bg-white text-[#9538E2] hover:bg-yellow-400 hover:text-black rounded-xl p-2  inline-block'>Shop Now</Link>
         
          </div>
